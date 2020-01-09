ae608e25b2c5c6af6798ace35ecbba05c4014fa8
AhnYoungSeon <46748334+AYoungSn@users.noreply.github.com>
2020-01-09 15:19:28 +0900

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, Sungshin!"
 
-@app.route("/hello")
+@app.route("/hi")
 def hello_world2():
-    return "Hello, Sungshin2!"
+    return "Hi, Sungshin2!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
